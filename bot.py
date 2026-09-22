4126c79974a7c42dc4f1e6f4c3325f22c2542cc4
emporomenkou <noobprohacker@tutamail.com>
2026-09-23 02:35:54 +0300

    gvvk

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
 import keyring
 import logging
 import ssl
+import json
+import os
+import csv
+from datetime import datetime
 from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.filters import CommandStart, or_f
@@ -25,12 +29,38 @@ class DisabledSSLAiohttpSession(AiohttpSession):
                 json_serialize=self.json_dumps, 
             )
         return self._session
-
-
-
+    
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
 logging.basicConfig(level=logging.INFO)
+LOG_FILE = 'bot_loggs.csv'
+def logger(
+    user_id: int,
+    username: str | None,
+    motion: str,
+    api: str = "NONE",
+    api_answer: str = "NONE",
+) :
+    nick = f"@{username}" if username else "None"
+    now = datetime.now()
+    date_str = now.strftime("%d.%m.%Y")
+    time_str = now.strftime("%H:%M:%S")
+
+    api_answer_clean = str(api_answer).replace("\n", " ").replace("\r", " ")[:500]
+
+    file_exists = os.path.exists(LOG_FILE)
+
+    with open(LOG_FILE, "a", encoding="utf-8", newline="") as f:
+        writer = csv.writer(f, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        if not file_exists:
+            writer.writerow(
+                ["Unic ID", "@TG nick", "Motion", "API", "Date", "Time", "API answer"]
+            )
+        writer.writerow(
+            [user_id, nick, motion, api, date_str, time_str, api_answer_clean]
+        )
+
+
 WEATHER_CODES = {
     0: "☀️ Ясно",
     1: "🌤 В основном ясно",
@@ -124,14 +154,22 @@ async def currency_convert(s: str):
             data = await response.json()
     if data.get("result") == "success":
         rate = data["conversion_rate"]
-        print(parts, rate)
         return f"{float(parts[0]) * rate} {parts[2]}"
     else:
-        return r"data.get('result') != success"
+        return r"ошибка"
 
 
 @dp.message(or_f(CommandStart(), F.text == "назад"))
-async def start_handler(message: Message):
+async def start_handler(message: Message, state: FSMContext):
+    motion = "start" if message.text and message.text.startswith("/") else "назад"
+    await state.clear()
+    logger(
+        message.from_user.id,
+        message.from_user.username,
+        motion=motion,
+        api="NONE",
+        api_answer="NONE",
+    )
     user_name = message.from_user.full_name
     
     await message.answer(f"Привет, {user_name}!\n"
@@ -139,19 +177,23 @@ async def start_handler(message: Message):
                         )
 
  
-@dp.message(F.text.in_(['погода', 'котеки', "конвертер валют", "апи4"]))
+@dp.message(F.text.in_(['погода', 'котеки', "конвертер валют", "смешнявка"]))
 async def button_handler(message : Message, state: FSMContext):
     button = message.text
-
+    id = message.from_user.id
+    username = message.from_user.username
+   
     if button in ['погода', 'конвертер валют']:
+        api = "NONE"
+        api_answer = "NONE"         
         await state.update_data(action=button)
         await state.set_state(InputState.waiting_for_data)
-    if button == "погода":
+    if button == "погода":      
         await message.answer(
             text="Введите корректное название города на русском языке: ",
               reply_markup=keyboard2
         )
-    elif button == "конвертер валют":
+    elif button == "конвертер валют":    
         await message.answer(
             text=f"Введите параметры конвертации.\nпример для перевода 100 USD в BYN:\n<b>100 usd byn</b>",
               parse_mode="HTML",
@@ -161,54 +203,82 @@ async def button_handler(message : Message, state: FSMContext):
     elif button == "котеки":
         async with aiohttp.ClientSession() as session:
             async with session.get(r"https://api.thecatapi.com/v1/images/search") as response:
-                if response.status != 200:
-                    await message.answer(text='koteka ne budet')
-                    return
                 data = await response.json()
                 cat_url = data[0]["url"]
+            api = "thecatapi"
+            api_answer = cat_url if cat_url else "NONE"
+
         await message.answer_photo(photo=cat_url,
                                     caption='randomny kotek'
                                     )
     elif button == "смешнявка":
-        url = "https://anekdot.me"
-        async with aiohttp.ClientSession as session:
+        url = "http://rzhunemogu.ru/RandJSON.aspx?CType=1"
+        async with aiohttp.ClientSession() as session:
             async with session.get(url, ssl=False) as response:
-                data = await response.json()
-                text = data["text"]
-                await message.answer(text=text)
+                text = await response.text(encoding='cp1251')
+                data = json.loads(text, strict=False)
+                joke = data.get('content')
+        api = "rzhunemogu"
+        api_answer = joke if joke else "NONE"
+        await message.answer(text=joke.strip())
+    logger(id,
+        username,
+        motion=button, 
+        api=api,
+        api_answer=api_answer)
+    
 @dp.message(InputState.waiting_for_data)
 async def process_data(message: Message, state: FSMContext):
     data = await state.get_data()
     action = data.get("action")
     user_input = message.text
-
+    id = message.from_user.id
+    username = message.from_user.username
     if user_input == "назад":
         await message.answer(text="Возвращаемся в меню...", reply_markup=keyboard1)
         await state.clear()
+        logger(id,
+               username,
+               motion='назад',
+               api="NONE",
+               api_answer="NONE")
         return
 
     elif action == "погода":
         text = await weather(user_input)
         await message.answer(text, parse_mode="HTML")
-
+        api = "open-meteo"
+        api_answer = text if text else "NONE"
 
     elif action == "конвертер валют":
         result = await currency_convert(user_input)
         await message.answer(result, parse_mode="HTML")
+        api = "exchangerate_api"
+        api_answer = result if result else "NONE"
 
+    logger(id,
+           username,
+           motion=action,
+           api=api,
+           api_answer=api_answer)
 
 @dp.message(~F.text.in_(["погода", "котеки", "конвертер валют", "смешнявка"]))
 async def other_messages(message: Message, state: FSMContext):
     current_state = await state.get_state()
-    
+    id = message.from_user.id
+    username = message.from_user.username    
     if current_state is None:
         await message.answer("нормально общайся")
-
+    logger(id,
+           username,
+           motion="non-handle message",
+           api="NONE",
+           api_answer="NONE")
 
 async def main():
     session = DisabledSSLAiohttpSession()
 
-    BOT_TOKEN = keyring.get_password(r't.me\krivoobot', 'token')
+    BOT_TOKEN = keyring.get_password(r't.me/krivoobot', 'token')
     bot = Bot(token=BOT_TOKEN, session=session)
 
     await dp.start_polling(bot)
